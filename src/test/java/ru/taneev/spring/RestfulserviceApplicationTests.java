072446e8ef1da5ada036c2a1eb5bc55a60d18ea1
taneev_ar <taneev_ar@interrao.ru>
2022-02-18 21:24:51 +0300

    initial commit

---
+++ src/test/java/ru/taneev/spring/RestfulserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taneev.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
